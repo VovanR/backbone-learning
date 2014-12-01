0b0ae89591f7a967dfc22cb641472721e05489bf
Vladimir Rodkin <r.vladimir@dlinksys.ru>
2014-12-01 14:44:31 +0300

    104

--- src/scripts/views/TodoView.js
+++ src/scripts/views/TodoView.js
@@ -10,20 +10,54 @@
         initialize: function () {
             console.info('TodoView#initialize');
             this.listenTo(this.model, 'change', this.render);
+            this.listenTo(this.model, 'destroy', this.remove);
+            this.listenTo(this.model, 'visible', this.toggleVisible);
         },
         /**
          */
         render: function () {
             console.log('TodoView#render');
             this.$el.html(this.template(this.model.toJSON()));
+            this.$el.toggleClass('completed', this.model.get('completed'));
+            this.toggleVisible();
             this.$input = this.$('.edit');
 
             return this;
         },
         events: {
+            'click .toggle': 'toggleCompleted',
             'dblclick label': 'edit',
+            'click .destroy': 'clear',
             'keypress .edit': 'updateOnEnter',
             'blur .edit': 'close',
+        },
+
+        /**
+         * Переключает видимость элемента
+         */
+        toggleVisible: function () {
+            console.log('TodoView#toggleVisible');
+            this.$el.toggleClass('hidden', this.isHidden());
+        },
+
+        /**
+         * Должен ли элемент быть скрытым
+         */
+        isHidden: function () {
+            console.log('TodoView#isHidden');
+            var isCompleted = this.model.get('completed');
+            return (
+                (!isCompleted && global.App.TodoFilter === 'completed') ||
+                (isCompleted && global.App.TodoFilter === 'active')
+            );
+        },
+
+        /**
+         * Переключает состояние `completed` модели
+         */
+        toggleCompleted: function () {
+            console.log('TodoView#toggleCompleted');
+            this.model.toggle();
         },
 
         /**
@@ -43,6 +77,8 @@
                 this.model.save({
                     title: value,
                 });
+            } else {
+                this.clear();
             }
             this.$el.removeClass('editing');
         },
@@ -55,6 +91,15 @@
             if (e.which === ENTER_KEY) {
                 this.close();
             }
+        },
+
+        /**
+         * Удаляет элемент, уничтожает модель
+         * в локальном хранилище и удаляет ее представление
+         */
+        clear: function () {
+            console.log('TodoView#clear');
+            this.model.destroy();
         },
     });
 
